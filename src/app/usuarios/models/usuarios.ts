e4190ca9d616a30824304856dc9b41abc9d4b03b
aleortbas-uniandes <a.ortizb23@uniandes.edu.co>
2026-04-27 22:44:19 -0500

    Correcion model usuarios

--- src/app/usuarios/models/usuarios.ts
+++ src/app/usuarios/models/usuarios.ts
@@ -3,11 +3,19 @@ export class Usuarios {
   username: string;
   name: string;
   email: string;
+  avatarUrl: string;
+  role: string;
+  location: string; 
+  reposIds: number[];
 
-  constructor(id: number, username: string, name: string, email: string) {
+  constructor(id: number, username: string, name: string, email: string, avatarUrl: string, role: string, location: string, reposIds: number[]) {
     this.id = id;
     this.username = username;
     this.name = name;
     this.email = email;
+    this.avatarUrl = avatarUrl;
+    this.role = role;
+    this.location = location;
+    this.reposIds = reposIds;
   }
 }
